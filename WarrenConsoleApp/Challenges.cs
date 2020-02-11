c8396bcb6e8cee7b1e1ac711f463674b1837a033
Warren Pitterson <warrenpittersondev@gmail.com>
2020-02-11 23:56:04 +0000

    Removed unused constructor

--- WarrenConsoleApp/Challenges.cs
+++ WarrenConsoleApp/Challenges.cs
@@ -17,11 +17,6 @@ namespace WarrenConsoleApp
             this.Solution = solution;
         }
 
-        public Challenges()
-        {
-        }
-
-
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
