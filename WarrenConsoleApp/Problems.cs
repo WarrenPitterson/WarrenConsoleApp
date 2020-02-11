639f950d2d0ca4061af4ede5991c6b976f7facb2
Warren Pitterson <warrenpittersondev@gmail.com>
2020-02-11 00:42:11 +0000

    c# console app - Euler

---
+++ WarrenConsoleApp/Problems.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarrenConsoleApp
+{
+    public class Problems
+    {
+        public Problems(int id, string name, string description, string solution)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Description = description;
+            this.Solution = solution;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Solution { get; set; }
+
+    }
+}
